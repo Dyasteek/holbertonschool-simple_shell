421e67e7dca03ff259be44c246738635b9e6cdaa
Dyasteek <lawarsenik@gmail.com>
2025-08-14 14:01:46 -0300

    Make all betty friendly

--- pre-shell/prompt.c
+++ pre-shell/prompt.c
@@ -1,31 +1,42 @@
 #include "prompt.h"
 
-int main(void) {
+/**
+ * main - Simple shell prompt that reads and displays commands
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *buffer;
+	size_t bufsize = 10;
+	ssize_t args;
 
-    char *buffer;
-    size_t bufsize = 10;
-    ssize_t args;
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable buffer");
+		exit(1);
+	}
 
-    buffer = (char *)malloc(bufsize * sizeof(char));
-    if (buffer == NULL) {
-        perror("Unable buffer");
-        exit(1);
-    }
+	while (1)
+	{
+		printf("$ ");
+		args = getline(&buffer, &bufsize, stdin);
 
-    while (1) {
-        printf("$ ");
-        args = getline(&buffer, &bufsize, stdin);
+		if (args == -1 || strcmp(buffer, "end of file\n") == 0 ||
+		    strcmp(buffer, "EOF\n") == 0)
+		{
+			printf("🏃\n");
+			break;
+		}
 
-        if (args == -1 || strcmp(buffer, "end of file\n") == 0 || strcmp(buffer, "EOF\n") == 0) {
-            printf("🏃\n");
-            break;
-        }
-        
-        char* token = strtok(buffer, "\n");
-        if (token != NULL) {
-            printf("%s\n", token);
-        }
-    }
-    free(buffer);
-    return (0);
+		char *token = strtok(buffer, "\n");
+
+		if (token != NULL)
+		{
+			printf("%s\n", token);
+		}
+	}
+	free(buffer);
+	return (0);
 }
